a27c3b736a86405d2a01b7c657c81aeeffde7c66
Jaime David Guevara Rojas <jaimeguevara@u.boisestate.edu>
2019-05-20 10:33:39 -0600

    Post-semester commit

---
+++ final_images/scrape_images_py3.py
@@ -0,0 +1,27 @@
+from imutils import paths
+import pandas as pd
+import requests
+import cv2
+import os
+from os import listdir
+from os.path import isfile, join
+import os.path as osp
+from tqdm import tqdm_notebook as tqdm
+from google_images_download import google_images_download
+
+img_folder = '.'
+
+def build_arguments(word):
+    args = {}
+    args['keywords'] = word
+    args['limit'] = 100
+    args['format'] = 'png'
+    #args['usage_rights'] = 'labeled-for-nocommercial-reuse'
+    return args
+
+response = google_images_download.googleimagesdownload()
+
+# Get Images 
+word = input("What images would you like to download?:  ")
+type(word)
+absolute_image_paths = response.download(build_arguments(word))
